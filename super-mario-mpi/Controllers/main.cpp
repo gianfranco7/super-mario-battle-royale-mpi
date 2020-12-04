96f5436cfe0c8f348c5c10443a8f1a367aeca657
gianfranco7 <gianfrancobagnarello@hotmail.com>
2020-12-03 23:01:17 -0600

    Added info to prints. Falta comms

--- super-mario-mpi/Controllers/main.cpp
+++ super-mario-mpi/Controllers/main.cpp
@@ -1,23 +1,52 @@
 #include "../Models/mario.cpp"
 #include "mpi.h"
+
+bool validate_entry(int chosen_id)
+{
+	if (chosen_id == 0)
+	{
+		return false;
+	}
+	else
+		return true;
+}
+
 int main(int argc, char *argv[])
 {
 	int my_id, num_processes, chosen_id;
-	if (argc >= 2)
+	char strategy;
+	if (argc == 3)
 	{
-		chosen_id = (size_t)strtoul(argv[1], NULL, 10);
+		if (validate_entry((size_t)strtoul(argv[1], NULL, 10)))
+		{
+			chosen_id = (size_t)strtoul(argv[1], NULL, 10);
+			strategy = *argv[2];
+		}
+		else
+		{
+			cout << "Strategy must be R L A or M, id cannot be 0" << endl;
+			return 1;
+		}
 	}
 	else
 	{
 		cout << "Invalid number of parameters" << endl;
-		return 1;
+		return 2;
 	}
 	MPI_Status status;
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &num_processes);
 	MPI_Comm_rank(MPI_COMM_WORLD, &my_id);
-	Mario mario(my_id,chosen_id);
-	mario.travelWorld();
+	if (my_id == chosen_id)
+	{
+		Mario chosen_mario(my_id, chosen_id, strategy);
+		chosen_mario.travelWorld();
+	}
+	else
+	{
+		Mario mario(my_id, chosen_id);
+		mario.travelWorld();
+	}
 	MPI_Finalize();
 	return 0;
 }
