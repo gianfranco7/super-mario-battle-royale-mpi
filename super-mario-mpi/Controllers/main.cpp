e48b6573777ccb7e3af84d453fdeed4dc777d1f4
gianfranco7 <gianfrancobagnarello@hotmail.com>
2020-12-03 20:32:16 -0600

    players now run in background

--- super-mario-mpi/Controllers/main.cpp
+++ super-mario-mpi/Controllers/main.cpp
@@ -1,8 +1,23 @@
 #include "../Models/mario.cpp"
-
-int main()
+#include "mpi.h"
+int main(int argc, char *argv[])
 {
-	Mario mario;
+	int my_id, num_processes, chosen_id;
+	if (argc >= 2)
+	{
+		chosen_id = (size_t)strtoul(argv[1], NULL, 10);
+	}
+	else
+	{
+		cout << "Invalid number of parameters" << endl;
+		return 1;
+	}
+	MPI_Status status;
+	MPI_Init(&argc, &argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &num_processes);
+	MPI_Comm_rank(MPI_COMM_WORLD, &my_id);
+	Mario mario(my_id,chosen_id);
 	mario.travelWorld();
+	MPI_Finalize();
 	return 0;
 }
